1aa2f7f68f03d4a4b2abf11edd99a4f1b612b464
Kiyrean Dyer-Allen <kda_100@yahoo.co.uk>
2022-08-11 13:19:48 +0100

    new local repo location commit

--- src/test/java/com/example/animalKingdomSpringBoot/AnimalKingdomSpringBootApplicationTests.java
+++ src/test/java/com/example/animalKingdomSpringBoot/AnimalKingdomSpringBootApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.animalKingdomSpringBoot;
 
-import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
--- src/test/java/com/example/animalKingdomSpringBoot/cucumber/cats/RunCucumberTest.java
+++ src/test/java/com/example/animalKingdomSpringBoot/cucumber/cats/RunCucumberTest.java
@@ -5,6 +5,8 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"}, features = "src/test/java/resources.cucumber")
+@CucumberOptions(plugin = {"pretty"}, features = "src/test/java/resources/cucumber", glue = "org.example" +
+        ".animalKingdomSpringBoot.cucumber" +
+        ".cats.BreedCatStepDefs.java")
 public class RunCucumberTest {
 }
